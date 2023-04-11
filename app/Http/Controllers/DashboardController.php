57d12262a7264da6503a787104e28044c4d8233f
Inthania Nadicika Kurniawan <nadacika17@gmail.com>
2023-04-11 19:42:46 +0700

    pagination

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     function index(){
-        return view(('layouts.template'));
+        return view('dashboard');
     }
 }
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -17,7 +17,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="/index2.html"><b>Admin</b>ZiBob PetShop</a>
+    <a href="/index2.html"><b>Admin</b> ZiBob PetShop</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
@@ -65,7 +65,7 @@
         <a href="forgot-password.html">I forgot my password</a>
       </p>
       <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
+        <a href="{{url('/register')}}" class="text-center">Register a new admin</a>
       </p>
     </div>
     <!-- /.login-card-body -->
