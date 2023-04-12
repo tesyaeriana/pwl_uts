39d5f0b377e1187d4ea9f7707bb203cd6219f4fa
Inthania Nadicika Kurniawan <nadacika17@gmail.com>
2023-04-12 09:42:17 +0700

    seeder

--- database/seeders/ProdukSeeder.php
+++ database/seeders/ProdukSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProdukSeeder extends Seeder
 {
@@ -14,6 +15,42 @@ class ProdukSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('produk')->insert([
+            [
+                'kode' => 'PK001',
+                'nama_produk' => 'LifeCat',
+                'kategori_produk' => 'Makanan basah kucing',
+                'harga' => '20000',
+                'stok' => '18'
+            ],
+            [
+                'kode' => 'SM001',
+                'nama_produk' => 'Fungizol',
+                'kategori_produk' => 'Sampo kucing anjing',
+                'harga' => '87000',
+                'stok' => '10'
+            ],
+            [
+                'kode' => 'PK002',
+                'nama_produk' => 'Royal Canin Kitten',
+                'kategori_produk' => 'Makanan kering kucing',
+                'harga' => '142000',
+                'stok' => '21'
+            ],
+            [
+                'kode' => 'AK001',
+                'nama_produk' => 'Grooming Comb Kit',
+                'kategori_produk' => 'Aksesoris',
+                'harga' => '230000',
+                'stok' => '15'
+            ],
+            [
+                'kode' => 'OB',
+                'nama_produk' => 'Albenworm Tablet',
+                'kategori_produk' => 'Obat cacing kucing anjing',
+                'harga' => '15000',
+                'stok' => '30'
+            ]
+            ]);
     }
 }
